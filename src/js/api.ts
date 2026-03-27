0ca143af2440a7165fbd87b901a2c079a2db539d
elychnell <elychnell@gmail.com>
2026-03-27 10:58:29 +0100

    (build): Moved API address to .env file

--- src/js/api.ts
+++ src/js/api.ts
@@ -1,13 +1,12 @@
 
-const API_ADDRESS = process.env?.['API_ADRESS'] ?? ''
-    
+const API_ADDRESS = import.meta.env.VITE_API_ADRESS
+ 
 /**
  * @function getPodcasts
  * @description Fetches podcast data from the SR API.
  * @returns {Promise<Array>} A promise resolving to an array of podcast objects.
  */
 
-console.log('API_ADRESS:', API_ADDRESS)
 export async function getPodcasts() {
     return await fetch(API_ADDRESS)
         .then((data) => data.json())

6ccc8b16080d4149ddedb35ee61db185177e926b
elychnell <elychnell@gmail.com>
2026-03-27 10:42:21 +0100

    (build): added .env file + needed configs

--- src/js/api.ts
+++ src/js/api.ts
@@ -1,13 +1,15 @@
+
+const API_ADDRESS = process.env?.['API_ADRESS'] ?? ''
+    
 /**
  * @function getPodcasts
  * @description Fetches podcast data from the SR API.
  * @returns {Promise<Array>} A promise resolving to an array of podcast objects.
  */
 
+console.log('API_ADRESS:', API_ADDRESS)
 export async function getPodcasts() {
-    return await fetch(
-        'https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false'
-    )
+    return await fetch(API_ADDRESS)
         .then((data) => data.json())
         .then((json) => json)
         .catch((error) => {
